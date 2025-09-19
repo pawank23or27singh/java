412a4a0dd5e1245d38842b49b22f054c99fc9eff
pawank23or27singh <psr212324@gmail.com>
2025-09-19 23:09:20 +0530

    add

---
+++ vari.java
@@ -0,0 +1,15 @@
+public class vari {
+    public static void main(String[] args) {
+        int a = 10; // integer variable
+        float b = 20.5f; // float variable
+        char c = 'A'; // character variable
+        String d = "Hello"; // string variable
+        boolean e = true; // boolean variable
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("String: " + d);
+        System.out.println("Boolean: " + e);
+    }
+}
